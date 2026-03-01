59198424297148b448652d4b715418aaffe00c0d
Michael Adrian Javier <michaeljavierwork@gmail.com>
2026-03-01 14:33:35 +0800

    Added Mantine provider to app

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { MantineProvider } from '@mantine/core'
 import App from './App.tsx'
+import '@mantine/core/styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </StrictMode>,
 )
