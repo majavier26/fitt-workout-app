97d55a01cc63bdb2b4cce0442cbc0e7a17f59ac0
Michael Adrian Javier <michaeljavierwork@gmail.com>
2026-03-01 17:07:24 +0800

    Initial commit

---
+++ app/src/assets/config/home.ts
@@ -0,0 +1,20 @@
+import type { ToplineCardProps } from '../../components/ui/ToplineCard';
+
+// Topline cards
+export const homeToplineCards: ToplineCardProps[] = [
+    {
+        title: 'Number of sessions',
+        value: 0,
+        description: 'this week'
+    },
+    {
+        title: 'Total number of sessions',
+        value: 234,
+        description: 'since Dec. 16, 2021'
+    },
+    {
+        title: 'Body weight',
+        value: '78.5 kg',
+        description: 'as of yesterday'
+    },
+];
