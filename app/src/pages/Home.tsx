9451d79b0285f6193e13d23e8da6f29c1d34f367
Michael Adrian Javier <michaeljavierwork@gmail.com>
2026-03-07 22:08:55 +0800

    Made home page more responsive

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,22 +1,36 @@
-import { SimpleGrid, Stack, Text} from "@mantine/core";
+import { Box, SimpleGrid, Grid, Stack, Title } from "@mantine/core";
+import useFetchUser from "../features/users/hooks/useFetchUser";
 import ToplineCard from "../components/ui/ToplineCard";
-import { homeToplineCards } from "../assets/config/home";
+import ComponentCard from "../components/ui/ComponentCard";
+import { homeToplineCards, homeComponentCards } from "../assets/config/home";
 
 const Home = () => {
-  return (
-    <>
-        <SimpleGrid
-            cols={{
-                base: 1,
-                lg: 3
-            }}
-        >
-            {homeToplineCards.map((props) => {
-                return <ToplineCard {...props} />
-            })}
-        </SimpleGrid>
-    </>
-  )
+    // Get the user
+    const user = useFetchUser();
+
+    return (
+        <Box p='md'>
+            <Grid>
+                <Grid.Col span={{base: 12, sm: 6, lg: 8}}>
+                    <Stack>
+                        <Title order={1}>{`${user.name}'s Fitness Journey`}</Title>
+                        <SimpleGrid cols={{ base: 1, lg: 3 }} >
+                            {homeToplineCards.map((props) => {
+                                return <ToplineCard key={props.title} {...props} />
+                            })}
+                        </SimpleGrid>
+                    </Stack>
+                </Grid.Col>
+                <Grid.Col span={{base: 12, sm: 6, lg: 4}}>
+                    <Stack>
+                        {homeComponentCards.map((props) => {
+                            return <ComponentCard key={props.title} {...props} />
+                        })}
+                    </Stack>
+                </Grid.Col>
+            </Grid>
+        </Box>
+    )
 }
 
 export default Home
