32237839194c9c437960eb26561690f5f011a902
Michael Adrian Javier <michaeljavierwork@gmail.com>
2026-03-08 09:21:12 +0800

    Added a box for the navbar user profile, and added padding to the stacks

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,9 +1,8 @@
-import { Stack, Burger, Text, Group } from '@mantine/core';
+import { Stack, Burger, Group, Box } from '@mantine/core';
 import NavbarUserProfile from '../features/users/components/NavbarUserProfile';
 import NavbarRouteDisplay from '../features/routes/components/NavbarRouteDisplay';
 import { routes } from '../features/routes/assets/config/routes';
 import type { NavbarControlProps } from '../assets/types/NavbarProps';
-import { FONT_WEIGHT } from '../assets/config/constants';
 
 const Navbar = ({
     mobileOpened,
@@ -11,24 +10,29 @@ const Navbar = ({
     toggleMobile,
     toggleDesktop
 }: NavbarControlProps) => {
-  // Coalesce the open state
   const opened = desktopOpened || mobileOpened;
 
   return (
-    <Stack justify='space-between' h='100%' >
+    <Stack 
+      justify='space-between' 
+      h='100%' 
+      style={{ overflow: 'hidden' }}>
         <Group px="xs" justify={opened ? 'flex-start' : 'center'}>
             <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
             <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
-            {/* <Text fw={FONT_WEIGHT.Bold} size='lg'>F</Text> */}
         </Group>
-        <Stack gap={0}>
+
+        <Stack gap="sm" px="xs"> 
           {routes.map((route) => {
             return <NavbarRouteDisplay key={route.title} {...route} isExpanded={opened} />
           })}
         </Stack>
-        <NavbarUserProfile isExpanded={opened} />
+
+        <Box px='xs'>
+          <NavbarUserProfile isExpanded={opened} />
+        </Box>
     </Stack>
   )
 }
 
-export default Navbar
+export default Navbar;
